603ed4b21c562bb832c55bcabc4c35406451d5f1
Roy <roygufo@gmail.com>
2020-08-02 21:15:10 -0500

    Add obstacle

--- Scripts/Powerup.gd
+++ Scripts/Powerup.gd
@@ -21,3 +21,8 @@ func _ready():
 
 func _on_Tween_tween_completed(object, key):
 	queue_free()
+
+
+func _on_PowerUp_area_entered(area):
+	if area.is_in_group("obstacles"):
+		position = Vector2(rand_range(0, screensize.x), rand_range(0, screensize.y))
